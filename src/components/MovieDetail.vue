5b77e48aa95528a40d3b46c73920c781781c7111
eleazer abbey <abbeyeleazer@gmail.com>
2024-10-15 13:20:38 +0200

    first commit

---
+++ src/components/MovieDetail.vue
@@ -0,0 +1,30 @@
+<script setup>
+import movies from '@/movies.json'
+
+// import { defineProps } from 'vue';
+defineProps({ movie: Object })
+</script>
+
+<template>
+  <div v-if="movie" class="z-40 text-white w-[53%] h-[50%] pt-20">    
+    <div class="text-6xl font-semibold">{{movie.name}}</div>
+    <div class="flex items-center text-lg pt-4">
+    <div>{{ movie.year }}</div>
+
+    <div class="relative">
+      <span class="absolute left-[2px] -top-[26px] text-5xl">.</span>
+      <span class="pl-4">{{ movie.genre.one }}</span>  
+    </div>
+    <div class="relative">
+      <span class="absolute left-[2px] -top-[26px] text-5xl">.</span>
+      <span class="pl-4">{{ movie.genre.two }}</span>  
+    </div>
+    <div class="relative">
+      <span class="absolute left-[2px] -top-[26px] text-5xl">.</span>
+      <span class="pl-4">{{ movie.genre.three }}</span>  
+    </div>
+  </div>
+    <div class="text-2xl pt-6"> {{ movie.description }}</div>
+  </div>
+</template>
+
